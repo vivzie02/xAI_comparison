bc8c7ff0ed0c7c44838ee3d72f59bd7aab7643e3
vivzie02 <101602263+vivzie02@users.noreply.github.com>
2024-03-12 13:47:27 +0100

    added output layer logic
    softmax activation function for output layer
    some status information printed

--- xAI_sound_classifier_simple/regular_classifier/main.py
+++ xAI_sound_classifier_simple/regular_classifier/main.py
@@ -6,10 +6,19 @@ from sklearn.preprocessing import MaxAbsScaler
 import os
 import librosa
 from scipy import stats
+import math
 
 # setups
 np.random.seed()
 scaler = MaxAbsScaler()
+
+
+def relu_activation(neuron_value):
+    return max(0, neuron_value)
+
+
+def softmax_activation(inputs):
+    return np.exp(inputs) / np.sum(np.exp(inputs))
 
 
 # single neuron, has single bias and a list of weights equal to the amount of inputs from the layer before
@@ -18,36 +27,44 @@ class Neuron:
         self.weights = np.random.randn(1, n_inputs)
         # for initial setup set bias as 0
         self.bias = 0
-        self.output = 0
-
-    def activation(self, neuron_value):
-        # ReLU for now, maybe change later
-        return max(0, neuron_value)
+        self.neuron_value = 0
 
     def forward(self, inputs):
-        neuron_value = np.dot(self.weights, inputs)[0] + self.bias
-        self.output = self.activation(neuron_value)
-        print("Neuron output: input/weights/bias/output")
-        print(inputs, self.weights, self.bias, self.output)
-        return self.output
+        self.neuron_value = np.dot(self.weights, inputs)[0] + self.bias
+        print("Neuron output: input/weights/bias/output_before_activation")
+        print(inputs, self.weights, self.bias, self.neuron_value)
+        return self.neuron_value
 
 
 # single layer, has a set amount of neurons
 class Layer:
-    def __init__(self, n_inputs, n_neurons, is_input_layer):
+    def __init__(self, n_inputs, n_neurons, is_input_layer, is_output_layer):
         self.neurons = []
         self.is_input_layer = is_input_layer
+        self.is_output_layer = is_output_layer
         for i in range(n_neurons):
             self.neurons.append(Neuron(n_inputs))
 
     def forward(self, inputs):
         if self.is_input_layer:
             return inputs
+        elif self.is_output_layer:
+            # as probability distribution (softmax)
+            return self.get_final_outputs(inputs)
 
         outputs = []
         for neuron in self.neurons:
-            outputs.append(neuron.forward(inputs))
+            outputs.append(relu_activation(neuron.forward(inputs)))
         return outputs
+
+    def get_final_outputs(self, inputs):
+        neuron_values = []
+        for neuron in self.neurons:
+            neuron_values.append(neuron.forward(inputs))
+
+        # subtract max value to avoid overflow in exp function, result doesn't change
+        neuron_values = neuron_values - max(neuron_values)
+        return softmax_activation(neuron_values)
 
 
 # entire network of multiple layers
@@ -56,25 +73,30 @@ class Network:
         self.layers = []
 
         # add input layer
-        self.layers.append(Layer(0, n_input_features, True))
+        self.layers.append(Layer(0, n_input_features,is_input_layer=True, is_output_layer=False))
 
         # add hidden layers
         n_inputs = n_input_features
         for i in range(n_hidden_layers):
             n_neurons = n_hidden_nodes[i]
-            self.layers.append(Layer(n_inputs, n_neurons, False))
+            self.layers.append(Layer(n_inputs, n_neurons, is_input_layer=False, is_output_layer=False))
             n_inputs = n_hidden_nodes[i]
 
         # add output layer
-        self.layers.append(Layer(n_inputs, n_output_classes, False))
+        self.layers.append(Layer(n_inputs, n_output_classes, is_input_layer=False, is_output_layer=True))
 
     def predict(self, inputs):
         print("predict on ", inputs)
-        test = inputs[:50]
-        for layer in self.layers:
-            test = layer.forward(test)
+        layer_result = inputs[:50]
 
-        print("Final results: ", test)
+        # genre
+        result_class = inputs['genre']
+        for layer in self.layers:
+            layer_result = layer.forward(layer_result)
+
+        # final result as class probabilities
+        print("sum", np.sum(layer_result))
+        print("Final results: ", layer_result)
 
     def print_network(self):
         G = nx.Graph()
@@ -119,6 +141,8 @@ if __name__ == "__main__":
     columns = ['audio_data', 'genre']
     data = []
     genres = []
+
+    print("Reading data files...")
     for folder, subs, files in os.walk('../archive (15)/Data/genres_original'):
         for filename in files:
             try:
@@ -144,6 +168,7 @@ if __name__ == "__main__":
     # remove outliers
     df_all_columns = df_all_columns[(np.abs(stats.zscore(df_all_columns.iloc[:, :50])) < 3).all(axis=1)]
 
+    print("Building network...")
     # build network
     layer_nodes = [10, 10]
     n_hidden_layers = len(layer_nodes)
@@ -151,6 +176,9 @@ if __name__ == "__main__":
     n_inputs = 50
 
     nn = Network(n_hidden_layers, n_inputs, n_output_classes, layer_nodes)
+
+    print("visualize network")
     nn.print_network()
 
+    print("Example on a single input...")
     nn.predict(df_all_columns.iloc[0])
