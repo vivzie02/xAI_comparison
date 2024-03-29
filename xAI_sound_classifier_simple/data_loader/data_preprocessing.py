1ca62f28760cd8397eab582405eb34c92e533689
vivzie02 <101602263+vivzie02@users.noreply.github.com>
2024-03-29 23:29:51 +0100

    changed extracted data to fit CNN
    removed statistical data (for now)
    only get mfcc, delta, stft, chroma
    get 4d tensor as array of images

--- xAI_sound_classifier_simple/data_loader/data_preprocessing.py
+++ xAI_sound_classifier_simple/data_loader/data_preprocessing.py
@@ -1,123 +1,51 @@
 import librosa
 import numpy as np
 import os
-import pandas as pd
-import scipy
-from sklearn.preprocessing import MinMaxScaler
+import torch
 
 
-class AudioFeatures:
-    def __init__(self):
-        # general info
-        self.file = None
-        self.sampling_rate = None
-        self.time_series = None
-        self.genre = None
-
-        self.energy = None
-        self.zero_crossings = None
-        self.tempo = None
-        self.mfcc = None
-        self.mfcc_delta = None
-
-        # statistical data
-        self.mean = None
-        self.std = None
-        self.maxv = None
-        self.minv = None
-        self.median = None
-        self.skew = None
-        self.kurt = None
-        self.q1 = None
-        self.q3 = None
-        self.iqr = None
-
-
-def get_wave_data(data_path):
-    raw_data = []
-
+# takes the path to the audio files and returns a 4D tensor - a 3d image for each file (audio_files, width, height, channels)
+def get_images(data_path):
+    max_width = 1000
+    max_height = 50
+    images = []
     for root, dirs, files in os.walk(data_path):
         for file in files:
             print("Getting features for " + file)
-            features = AudioFeatures()
-
             path = os.path.join(root, file)
             genre = os.path.basename(root)
             # sr is the sampling rate, y is the audio time series
             y, sr = librosa.load(path)
 
-            features.file = file
-            features.genre = genre
-            features.sampling_rate = sr
-            features.time_series = y
+            # mfcc
+            mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=50, n_fft=255, hop_length=512, n_mels=20)
+            mfcc = mfcc[:max_height, :max_width]
+            # delta mfcc
+            delta_mfcc = librosa.feature.delta(mfcc)
+            # STFT (short time fourier)
+            stft = np.abs(librosa.stft(y, n_fft=255))
+            stft = stft[:max_height, :max_width]
+            # chroma stft
+            chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+            chroma_stft = chroma_stft[:max_height, :max_width]
 
-            features.energy = np.sum(y ** 2)
-            features.zero_crossings = sum(librosa.zero_crossings(y, pad=False))
-            features.tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+            mfcc_padded = np.pad(mfcc, ((0, max_height - mfcc.shape[0]), (0, max_width - mfcc.shape[1])), mode='constant')
+            delta_mfcc_padded = np.pad(delta_mfcc, ((0, max_height - delta_mfcc.shape[0]), (0, max_width - delta_mfcc.shape[1])), mode='constant')
+            stft_padded = np.pad(stft, ((0, max_height - stft.shape[0]), (0, max_width - stft.shape[1])), mode='constant')
+            chroma_stft_padded = np.pad(chroma_stft, ((0, max_height - chroma_stft.shape[0]), (0, max_width - chroma_stft.shape[1])), mode='constant')
 
-            # MFCC features (mean of time windows)
-            mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=50)
-            features.mfcc = mfcc
-            features.mfcc_delta = librosa.feature.delta(mfcc)
+            images.append((mfcc_padded, delta_mfcc_padded, stft_padded, chroma_stft_padded))
 
-            raw_data.append(features)
-
-    return raw_data
-
-
-def get_statistical_features(audio_data):
-    for file_data in audio_data:
-        print("Get stats for", file_data)
-
-        # get the amplitudes via fft (abs)
-        magnitudes = np.abs(np.fft.fft(file_data.time_series))
-
-        file_data.mean = np.mean(magnitudes)
-        file_data.std = np.std(magnitudes)
-        file_data.maxv = np.amax(magnitudes)
-        file_data.minv = np.amin(magnitudes)
-        file_data.median = np.median(magnitudes)
-        file_data.skew = scipy.stats.skew(magnitudes)
-        file_data.kurt = scipy.stats.kurtosis(magnitudes)
-        file_data.q1 = np.quantile(magnitudes, 0.25)
-        file_data.q3 = np.quantile(magnitudes, 0.75)
-        file_data.iqr = scipy.stats.iqr(magnitudes)
-
-    return audio_data
-
-
-def get_features(data_path):
-    # extract audio time series from the files (raw wave data)
-    audio_data = get_wave_data(data_path)
-
-    # extract statistical features from wave data
-    audio_data = get_statistical_features(audio_data)
-
-    df = pd.DataFrame.from_records(vars(data) for data in audio_data)
-    return df
-
-
-def preprocess_data(df):
-    # normalize
-    scaler = MinMaxScaler()
-    non_normalized_cols = ['sampling_rate', 'file', 'genre', 'mfcc']
-    # regular normalization
-    for column in df.columns:
-        if column in non_normalized_cols:
-            continue
-        df[column] = scaler.fit_transform(df[column])
-
-    return df
+    return np.array(images)
 
 
 def main():
     data_path = './archive (15)/Data/genres_original/'
 
-    # get MFCC, energy, ...
-    df = get_features(data_path)
+    images = get_images(data_path)
+    tensor = torch.tensor(images)
 
-    # data preprocessing
-    df = preprocess_data(df)
+    print("done")
 
 
 if __name__ == "__main__":
